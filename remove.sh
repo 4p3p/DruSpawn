b3421c0430bcc19dcb2bc4a64a0a30365ee078a9
root <root@fcg>
2016-10-31 15:14:31 -0600

    >_<

--- remove.sh
+++ remove.sh
@@ -10,6 +10,20 @@
 #	es un buen cliche...	#
 #***************************#
 
+sudo rm -rf /opt/drustroyer
+for dir in /home/*/
+do
+	sudo rm -rf $dir.drustroyer
+done
+sudo rm -rf /root/.drustroyer
+sudo rm -rf /bin/drustroyer
+
 sudo rm -rf /opt/druspawn
-sudo rm -rf ~/.druspawn
+for dir in /home/*/
+do
+	sudo rm -rf $dir.druspawn
+done
+sudo rm -rf /root/.druspawn
 sudo rm -rf /bin/druspawn
+
+echo "DruSpawn desinstalado de manera exitosa..."
